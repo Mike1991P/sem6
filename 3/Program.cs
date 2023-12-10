eb1c3461df0e09cf0c49b4544a86aadd53d6e01d
MIKE <kpocobok.muxa@mail.ru>
2023-12-10 10:54:19 +0300

    правки в решение

---
+++ 3/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Task3
+{
+public static void Main(string[] args)
+{
+// Входная строка для проверки
+string input = "шалаш";
+// Вызов метода для проверки, является ли строка палиндромомbool isPalindrome = IsPalindrome(input);
+// Вывод результата
+Console.WriteLine(IsPalindrome ? "Да" : "Нет");
+}
+
+string normalized = new
+string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+Return normalized.SequenceEqual(Normalized,Reverse());
+
+}
